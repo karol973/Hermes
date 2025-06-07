fb62b586e37b74708a770e713a88f653825e5976
Karol Przybysz <przybyszkarol97@gmail.com>
2025-06-07 21:08:09 +0200

    Add user controller

---
+++ src/Hermes.Api.Web/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using Hermes.Modules.Users.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hermes.Api.Web.Controllers
+{
+    [Route("api/user")]
+    public class UserController : ApiControllerBase
+    {
+        public UserController(IMediator mediator) : base(mediator)
+        {
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public Task<IActionResult> GetAllAsync()
+        {
+            return HandleAsync(new GetAllUsersQuery());
+        }
+    }
+}
