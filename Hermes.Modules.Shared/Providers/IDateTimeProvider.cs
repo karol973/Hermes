7d7c9f1901687770e37703edfac0c8c221869f15
Karol Przybysz <przybyszkarol97@gmail.com>
2025-06-07 21:02:22 +0200

    AddProviders

---
+++ Hermes.Modules.Shared/Providers/IDateTimeProvider.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hermes.Modules.Shared.Providers
+{
+    public interface IDateTimeProvider : IProvider
+    {
+        Task<DateTime> GetDateTimeAsync(CancellationToken cancellationToken);
+    }
+}
+
---
+++ Hermes.Modules.Shared/Providers/IProvider.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Hermes.Modules.Shared.Providers
+{
+   /// <summary>
+   /// Marker interface
+   /// </summary>
+   public interface IProvider
+   { }
+}
