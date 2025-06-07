03f79c400e4a3bca508ebc56d89c88a550d3d2a1
Karol Przybysz <przybyszkarol97@gmail.com>
2025-06-07 21:07:36 +0200

    Add user queries

---
+++ src/Modules/Hermes.Modules.Administration/Queries/GetAllUsersQuery.cs
@@ -0,0 +1,9 @@
+﻿using Hermes.Application.DTOs;
+using MediatR;
+
+namespace Hermes.Modules.Users.Queries
+{
+    public sealed class GetAllUsersQuery : IRequest<IEnumerable<UserDto>>
+    {
+    }
+}
---
+++ src/Modules/Hermes.Modules.Administration/Queries/GetAllUsersQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AutoMapper;
+using Hermes.Application.DTOs;
+using Hermes.Domain.Entities;
+using Hermes.Modules.Shared.Handlers;
+using Hermes.Modules.Shared.Providers;
+using Infrastructure.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hermes.Modules.Users.Queries
+{
+    public sealed class GetAllUsersQueryHandler : HandlerBase, IRequestHandler<GetAllUsersQuery, IEnumerable<UserDto>>
+    {
+        public GetAllUsersQueryHandler(IMapper mapper, IDateTimeProvider dateTimeProvider, AntiqueShopDbContext antiqueShopDbContext) : base(mapper, dateTimeProvider, antiqueShopDbContext)
+        {
+        }
+
+        public async Task<IEnumerable<UserDto>> Handle(GetAllUsersQuery request, CancellationToken cancellationToken)
+        {
+            var users = await _context.Users
+                .Include(u => u.Address)
+                .ToListAsync(cancellationToken);
+            return _mapper.Map<IEnumerable<UserDto>>(users);
+
+        }
+    }
+}
