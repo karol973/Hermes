cd1af57d83a8d97c2fbb21d92a58d0af50b8357a
Karol Przybysz <przybyszkarol97@gmail.com>
2025-06-07 21:06:42 +0200

    Add ApiControllerBase

---
+++ src/Hermes.Api.Web/Controllers/ApiControllerBase.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hermes.Api.Web.Controllers
+{
+    [ApiController]
+    public abstract class ApiControllerBase : ControllerBase
+    {
+        protected readonly IMediator _mediator;
+
+        public ApiControllerBase(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        protected async Task<IActionResult> HandleAsync(IBaseRequest request)
+        {
+            return Ok(await _mediator.Send(request, HttpContext.RequestAborted));
+        }
+    }
+}
