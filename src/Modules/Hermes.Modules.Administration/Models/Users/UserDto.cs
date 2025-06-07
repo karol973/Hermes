073e2d079a7986fc1914b7c7d9efb94e1c459966
Karol Przybysz <przybyszkarol97@gmail.com>
2025-06-07 21:07:00 +0200

    Add UserDto

---
+++ src/Modules/Hermes.Modules.Administration/Models/Users/UserDto.cs
@@ -0,0 +1,19 @@
+﻿using Hermes.Domain.Enums;
+
+namespace Hermes.Application.DTOs
+{
+    public class UserDto
+    {
+        public int Id { get; set; }
+        public string Username { get; set; } = string.Empty;
+        //public Role Role { get; set; }
+        public string Role { get; set; } = string.Empty; 
+        public bool IsActive { get; set; }
+        public int? AddressId { get; set; }
+        public string? AddressStreet { get; set; }
+        public string? AddressCity { get; set; }
+        public string? AddressPostalCode { get; set; }
+        public string? AddressCountry { get; set; }
+
+    }
+}
