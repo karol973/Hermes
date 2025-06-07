74b947b2b3cd1cbf5e16e6ef600c4930189a3504
Karol Przybysz <przybyszkarol97@gmail.com>
2025-06-07 21:02:07 +0200

    Add HandlerBase

---
+++ Hermes.Modules.Shared/Handlers/HandlerBase.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using Hermes.Modules.Shared.Providers;
+using Infrastructure.Persistence;
+
+namespace Hermes.Modules.Shared.Handlers
+{
+    public abstract class HandlerBase
+    {
+        protected readonly IMapper _mapper;
+        protected readonly IDateTimeProvider _dateTimeProvider;
+        protected readonly AntiqueShopDbContext _context;
+
+
+        public HandlerBase(IMapper mapper, IDateTimeProvider dateTimeProvider, AntiqueShopDbContext antiqueShopDbContext)
+        {
+            _mapper = mapper;
+            _dateTimeProvider = dateTimeProvider;
+            _context = antiqueShopDbContext;
+        }
+    }
+}
