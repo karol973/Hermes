1f2b384b782c2b537c848fda0cbbee41546324b9
Karol Przybysz <przybyszkarol97@gmail.com>
2025-06-07 16:53:05 +0200

    Add enums

---
+++ src/Hermes.Shared/Enums/EnumExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hermes.Shared.Enums
+{
+    public static class EnumExtensions
+    {
+        public static string GetDescription(this Enum value)
+        {
+            Type type = value.GetType();
+            string name = Enum.GetName(type, value);
+
+            if (!string.IsNullOrEmpty(name))
+            {
+                FieldInfo field = type.GetField(name);
+                if (field != null)
+                {
+                    DescriptionAttribute descriptionAttribute = (DescriptionAttribute)Attribute.GetCustomAttribute(field, typeof(DescriptionAttribute));
+                    if (descriptionAttribute != null)
+                    {
+                        return descriptionAttribute.Description;
+                    }
+                }
+            }
+
+            return string.Empty;
+        }
+    }
+}
---
+++ src/Hermes.Shared/Enums/EnumHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hermes.Shared.Enums
+{
+    public static class EnumHelper<T> where T : Enum
+    {
+        public static IEnumerable<KeyValuePair<int, string>> GetValues(bool skipDefault = false)
+        {
+            IEnumerable<T> values = Enum.GetValues(typeof(T)).Cast<T>();
+
+            if (skipDefault)
+            {
+                values = values.Where(e => Convert.ToInt32(e) != 0);
+            }
+
+            return values
+               .Select(e => new KeyValuePair<int, string>(Convert.ToInt32(e), ((Enum)e).GetDescription()))
+               .ToList();
+        }
+    }
+}
