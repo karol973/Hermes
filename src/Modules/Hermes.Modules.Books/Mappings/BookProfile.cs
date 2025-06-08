cf617629c3f7fec034b7279e5a92ea2b27975da0
Karol Przybysz <przybyszkarol97@gmail.com>
2025-06-08 18:35:26 +0200

    Add queries and mappings

---
+++ src/Modules/Hermes.Modules.Books/Mappings/BookProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Hermes.Domain.Entities;
+using Hermes.Modules.Books.Models;
+using Hermes.Shared.Enums;
+
+
+namespace Hermes.Modules.Books.Mappings
+{
+    internal sealed class BookProfile : Profile
+    {
+        public BookProfile()
+        {
+            CreateMap<Book, BookDto>()
+                .ForMember(dest => dest.Category, opt => opt.MapFrom((src, dest) => src.Category.GetDescription()))
+                .ForMember(dest => dest.AuthorName, opt => opt.MapFrom(src => src.Author != null ? src.Author.Name : ""))
+                .ForMember(dest => dest.PublisherName, opt => opt.MapFrom(src => src.Publisher != null ? src.Publisher.Name : ""))
+                .ForMember(dest => dest.AntiqueShopName, opt => opt.MapFrom(src => src.AntiqueShop != null ? src.AntiqueShop.Name : ""))
+                .ForMember(dest => dest.OrderItemsCount, opt => opt.MapFrom(src => src.OrderItems != null ? src.OrderItems.Count : 0));
+
+        }
+    }
+}
---
+++ src/Modules/Hermes.Modules.Books/Queries/GetAllBooksQuery.cs
@@ -0,0 +1,9 @@
+﻿using Hermes.Modules.Books.Models;
+using MediatR;
+
+namespace Hermes.Modules.Books.Queries
+{
+    public sealed class GetAllBooksQuery : IRequest<IEnumerable<BookDto>>
+    {
+    }
+}
