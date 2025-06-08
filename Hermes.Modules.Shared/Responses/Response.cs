f80b150394d94ac7afd0deeb1e4bc6f5a8a1efaf
Karol Przybysz <przybyszkarol97@gmail.com>
2025-06-08 13:17:17 +0200

    Add response

---
+++ Hermes.Modules.Shared/Responses/Response.cs
@@ -0,0 +1,13 @@
+﻿namespace Hermes.Modules.Shared.Response
+{
+    public class Response
+    {
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; }
+        public long ObjectOid { get; set; }
+
+        public static Response Success() => new() { IsSuccess = true };
+        public static Response Success(long oid) => new() { IsSuccess = true, ObjectOid = oid };
+        public static Response Failure(string message) => new() { IsSuccess = false, Message = message };
+    }
+}
