1dbf298a0b3d91bb2f2ec8366806bbdfac61deda
Karol Przybysz <przybyszkarol97@gmail.com>
2025-06-13 00:39:48 +0200

    Add update book command and handler

---
+++ src/Modules/Hermes.Modules.Books/Commands/Books/UpdateBook/UpdateBookCommand.cs
@@ -0,0 +1,21 @@
+﻿using Hermes.Domain.Enums;
+using Hermes.Modules.Shared.Response;
+using MediatR;
+
+namespace Hermes.Modules.Books.Commands.Books.UpdateBook
+{
+    public sealed class UpdateBookCommand : IRequest<Response>
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; } = string.Empty;
+        public int PublishYear { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public bool IsAvailable { get; set; }
+        public byte[]? BookImage { get; set; }
+        public int AuthorId { get; set; }
+        public int PublisherId { get; set; }
+        public BookCategory Category { get; set; } = BookCategory.None;
+        public int AntiqueShopId { get; set; } = 1;
+    }
+}
---
+++ src/Modules/Hermes.Modules.Books/Commands/Books/UpdateBook/UpdateBookCommandHandler.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using Hermes.Domain.Entities;
+using Hermes.Modules.Shared.Handlers;
+using Hermes.Modules.Shared.Providers;
+using Hermes.Modules.Shared.Response;
+using Infrastructure.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hermes.Modules.Books.Commands.Books.UpdateBook
+{
+    public class UpdateBookCommandHandler : HandlerBase, IRequestHandler<UpdateBookCommand, Response>
+    {
+        public UpdateBookCommandHandler(IMapper mapper, IDateTimeProvider dateTimeProvider, AntiqueShopDbContext antiqueShopDbContext) : base(mapper, dateTimeProvider, antiqueShopDbContext)
+        {
+        }
+
+        public async Task<Response> Handle(UpdateBookCommand request, CancellationToken cancellationToken)
+        {
+            Book bookToUpdate = await _context.Books.FirstOrDefaultAsync(b => b.Id == request.Id, cancellationToken);
+
+            if (bookToUpdate is null)
+            {
+                return Response.Failure($"Book with name '{request.Id}' and Id already exists.");
+            }
+
+            DateTime currentDate = await _dateTimeProvider.GetDateTimeAsync(cancellationToken);
+
+            bookToUpdate.Name = request.Name;
+            bookToUpdate.PublishYear = request.PublishYear;
+            bookToUpdate.Price = request.Price;
+            bookToUpdate.Quantity = request.Quantity;
+            bookToUpdate.IsAvailable = request.IsAvailable;
+            bookToUpdate.BookImage = request.BookImage;
+            bookToUpdate.AuthorId = request.AuthorId;
+            bookToUpdate.PublisherId = request.PublisherId;
+            bookToUpdate.Category = request.Category;
+            bookToUpdate.AntiqueShopId = request.AntiqueShopId;
+            bookToUpdate.ModifyDate = currentDate;
+
+            await _context.SaveChangesAsync();
+
+            return Response.Success(bookToUpdate.Id);
+
+        }
+    }
+}
