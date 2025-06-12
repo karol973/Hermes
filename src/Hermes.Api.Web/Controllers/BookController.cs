28f2b0fa5c47fa4da25e3958c574bdab7eb332da
Karol Przybysz <przybyszkarol97@gmail.com>
2025-06-13 00:42:17 +0200

    Add book controller

--- src/Hermes.Api.Web/Controllers/BookController.cs
+++ src/Hermes.Api.Web/Controllers/BookController.cs
@@ -1,12 +1,13 @@
 ﻿using Hermes.Modules.Books.Commands.Books.CreateBook;
-using Hermes.Modules.Books.Queries;
+using Hermes.Modules.Books.Queries.GetAllBooks;
+using Hermes.Modules.Books.Queries.GetBookById;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Hermes.Api.Web.Controllers
 {
-    [Route("api/book")]
+    [Route("api/books")]
     public class BookController : ApiControllerBase
     {
         public BookController(IMediator mediator) : base(mediator)
@@ -18,6 +19,13 @@ namespace Hermes.Api.Web.Controllers
         public Task<IActionResult> GetAllAsync()
         {
             return HandleAsync(new GetAllBooksQuery());
+        }
+
+        [HttpGet("{id:int}/bookdetails")]
+        [AllowAnonymous]
+        public Task<IActionResult> GetBookByIdAsync(int id)
+        {
+            return HandleAsync(new GetBookByIdQuery() { Id = id });
         }
 
         [HttpPost]
