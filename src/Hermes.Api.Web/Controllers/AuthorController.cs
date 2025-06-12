828c5b4227ab2f0ee28c3a018e644aacbfe4b324
Karol Przybysz <przybyszkarol97@gmail.com>
2025-06-13 00:42:39 +0200

    add author controller

---
+++ src/Hermes.Api.Web/Controllers/AuthorController.cs
@@ -0,0 +1,22 @@
+﻿using Hermes.Modules.Authors.Queries.GetAllAuthors;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hermes.Api.Web.Controllers
+{
+    [Route("api/authors")]
+    public class AuthorController : ApiControllerBase
+    {
+        public AuthorController(IMediator mediator) : base(mediator)
+        {
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public Task<IActionResult> GetAllAuthors()
+        {
+            return HandleAsync(new GetAllAuthorsQuery());
+        }
+    }
+}
