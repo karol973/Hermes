30224efe7c296cff7e79dad24b7a679a249e1545
Karol Przybysz <przybyszkarol97@gmail.com>
2025-06-13 00:41:16 +0200

    add mappings

---
+++ src/Modules/Hermes.Modules.Authors/Mappings/AuthorProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Hermes.Domain.Entities;
+using Hermes.Modules.Authors.Models;
+
+namespace Hermes.Modules.Authors.Mappings
+{
+    internal sealed class AuthorProfile : Profile
+    {
+        public AuthorProfile()
+        {
+            CreateMap<Author, AuthorDto>()
+                .ForMember(dest => dest.Books, opt => opt.MapFrom(src => src.Books));
+
+        }
+    }
+}
