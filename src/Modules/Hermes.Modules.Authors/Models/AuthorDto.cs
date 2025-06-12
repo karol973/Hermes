fef5ff6c4545c629c7d18364fba109da6f6bae4c
Karol Przybysz <przybyszkarol97@gmail.com>
2025-06-13 00:40:56 +0200

    Add author dto

---
+++ src/Modules/Hermes.Modules.Authors/Models/AuthorDto.cs
@@ -0,0 +1,12 @@
+﻿using Hermes.Modules.Books.Models;
+
+namespace Hermes.Modules.Authors.Models
+{
+    public class AuthorDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Surname { get; set; } = string.Empty;
+        public List<BookDto> Books { get; set; } = new();
+    }
+}
