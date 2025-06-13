edd9699703f9c1380a1361401bc04b9c1405cfef
Karol Przybysz <przybyszkarol97@gmail.com>
2025-06-13 07:48:38 +0200

    Add migration

--- src/Hermes.Infrastructure/Migrations/20250608163945_FixCategory.Designer.cs
+++ src/Hermes.Infrastructure/Migrations/20250608163945_FixCategory.Designer.cs
@@ -4,6 +4,7 @@ using Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Hermes.Infrastructure.Migrations
 {
     [DbContext(typeof(AntiqueShopDbContext))]
-    partial class AntiqueShopDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250608163945_FixCategory")]
+    partial class FixCategory
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -157,10 +160,6 @@ namespace Hermes.Infrastructure.Migrations
 
                     b.Property<int>("AuthorId")
                         .HasColumnType("int");
-
-                    b.Property<byte[]>("BookImage")
-                        .IsRequired()
-                        .HasColumnType("varbinary(max)");
 
                     b.Property<int>("Category")
                         .HasColumnType("int");
---
+++ src/Hermes.Infrastructure/Migrations/20250608163945_FixCategory.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hermes.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixCategory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Category",
+                table: "Books",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Category",
+                table: "Books",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}
