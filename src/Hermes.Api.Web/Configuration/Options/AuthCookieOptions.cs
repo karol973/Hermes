1928d50e291a8af9ed5340d509446efa2c550a63
Karol Przybysz <przybyszkarol97@gmail.com>
2025-06-19 22:39:44 +0200

    add auth cookie

---
+++ src/Hermes.Api.Web/Configuration/Options/AuthCookieOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace Hermes.Api.Web.Configuration.Options
+{
+    internal sealed class AuthCookieOptions
+    {
+        public string Issuer { get; set; }
+        public int ExpirationMinutes { get; set; }
+        public int AbsoluteExpirationMinutes { get; set; }
+    }
+}
