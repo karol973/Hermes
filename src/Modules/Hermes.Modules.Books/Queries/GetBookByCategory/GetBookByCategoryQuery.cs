34c622516f946240b766081763e11ce96d9ef2d0
Karol Przybysz <przybyszkarol97@gmail.com>
2025-06-19 20:27:41 +0200

    Add get book by category query and handler

---
+++ src/Modules/Hermes.Modules.Books/Queries/GetBookByCategory/GetBookByCategoryQuery.cs
@@ -0,0 +1,11 @@
+﻿using Hermes.Domain.Enums;
+using Hermes.Modules.Books.Models;
+using MediatR;
+
+namespace Hermes.Modules.Books.Queries.GetBooksByCategory
+{
+    public sealed class GetBookByCategoryQuery : IRequest<IEnumerable<BookDto>>
+    {
+        public BookCategory Category { get; set; }
+    }
+}
---
+++ src/Modules/Hermes.Modules.Books/Queries/GetBookByCategory/GetBookByCategoryQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using Hermes.Domain.Entities;
+using Hermes.Modules.Books.Models;
+using Hermes.Modules.Books.Queries.GetBooksByCategory;
+using Hermes.Modules.Shared.Handlers;
+using Hermes.Modules.Shared.Providers;
+using Infrastructure.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hermes.Modules.Books.Queries.GetBookByCategory
+{
+    public sealed class GetBookByCategoryQueryHandler : HandlerBase, IRequestHandler<GetBookByCategoryQuery, IEnumerable<BookDto>>
+    {
+        public GetBookByCategoryQueryHandler(IMapper mapper, IDateTimeProvider dateTimeProvider, AntiqueShopDbContext antiqueShopDbContext) : base(mapper, dateTimeProvider, antiqueShopDbContext)
+        {
+        }
+
+        public async Task<IEnumerable<BookDto>> Handle(GetBookByCategoryQuery request, CancellationToken cancellationToken)
+        {
+            IEnumerable<Book> books = await _context.Books
+                .Where(b => b.Category == request.Category)
+                .OrderBy(b => b.Name)
+                .ToListAsync();
+
+            return _mapper.Map<IEnumerable<BookDto>>(books);
+        }
+    }
+}
