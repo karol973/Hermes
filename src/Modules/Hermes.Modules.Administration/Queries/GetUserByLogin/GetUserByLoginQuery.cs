6bf5d80b5e6c26d96a85bb2405595fa6dead4210
Karol Przybysz <przybyszkarol97@gmail.com>
2025-06-19 22:39:13 +0200

    Add get user by login query

---
+++ src/Modules/Hermes.Modules.Administration/Queries/GetUserByLogin/GetUserByLoginQuery.cs
@@ -0,0 +1,15 @@
+﻿using Hermes.Application.DTOs;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hermes.Modules.Administration.Queries.GetUserByLogin
+{
+    public sealed class GetUserByLoginQuery : IRequest<UserDto>
+    {
+       public string Username { get; init; }
+    }
+}
---
+++ src/Modules/Hermes.Modules.Administration/Queries/GetUserByLogin/GetUserByLoginQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using Hermes.Application.DTOs;
+using Hermes.Domain.Entities;
+using Hermes.Modules.Shared.Handlers;
+using Hermes.Modules.Shared.Providers;
+using Infrastructure.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hermes.Modules.Administration.Queries.GetUserByLogin
+{
+    public sealed class GetUserByLoginQueryHandler : HandlerBase, IRequestHandler<GetUserByLoginQuery, UserDto>
+    {
+        public GetUserByLoginQueryHandler(IMapper mapper, IDateTimeProvider dateTimeProvider, AntiqueShopDbContext antiqueShopDbContext) : base(mapper, dateTimeProvider, antiqueShopDbContext)
+        {
+        }
+
+        public async Task<UserDto> Handle(GetUserByLoginQuery request, CancellationToken cancellationToken)
+        {
+            User existingUser = await _context.Users.FirstOrDefaultAsync(u => u.Username == request.Username, cancellationToken);
+
+            return _mapper.Map<UserDto>(existingUser);
+        }
+    }
+}
