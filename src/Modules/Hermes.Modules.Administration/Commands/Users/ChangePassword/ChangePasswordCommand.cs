96b2a66a0c985c308d4d555c9a8317f8222ce585
Karol Przybysz <przybyszkarol97@gmail.com>
2025-06-22 12:08:52 +0200

    Add change password command and handler

---
+++ src/Modules/Hermes.Modules.Administration/Commands/Users/ChangePassword/ChangePasswordCommand.cs
@@ -0,0 +1,13 @@
+﻿using Hermes.Domain.Enums;
+using Hermes.Modules.Shared.Response;
+using MediatR;
+
+namespace Hermes.Modules.Administration.Commands.Users.ChangePassword
+{
+    public sealed class ChangePasswordCommand : IRequest<Response>
+    {
+        public int Id { get; set; }
+        public string PasswordHash { get; set; } = string.Empty;
+
+    }
+}
---
+++ src/Modules/Hermes.Modules.Administration/Commands/Users/ChangePassword/ChangePasswordCommandHandler.cs
@@ -0,0 +1,40 @@
+﻿
+using AutoMapper;
+using Hermes.Domain.Entities;
+using Hermes.Modules.Shared.Handlers;
+using Hermes.Modules.Shared.Providers;
+using Hermes.Modules.Shared.Response;
+using Infrastructure.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hermes.Modules.Administration.Commands.Users.ChangePassword
+{
+    public sealed class ChangePasswordCommandHandler : HandlerBase, IRequestHandler<ChangePasswordCommand, Response>
+    {
+        public ChangePasswordCommandHandler(IMapper mapper, IDateTimeProvider dateTimeProvider, AntiqueShopDbContext antiqueShopDbContext) : base(mapper, dateTimeProvider, antiqueShopDbContext)
+        {
+        }
+
+        public async Task<Response> Handle(ChangePasswordCommand request, CancellationToken cancellationToken)
+        {
+            User user = await _context.Users.FirstOrDefaultAsync(u => u.Id == request.Id, cancellationToken);
+
+            if (user is null)
+            {
+                return Response.Failure($"User with a name: '{request.Id}' does not exist.");
+            }
+
+             user.PasswordHash = request.PasswordHash;
+ 
+            await _context.SaveChangesAsync();
+
+            return Response.Success();
+        }
+    }
+}
