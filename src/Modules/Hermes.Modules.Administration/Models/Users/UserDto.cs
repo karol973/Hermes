8e6acbb29e3139dada990da0b0a75abca3c53231
Karol Przybysz <przybyszkarol97@gmail.com>
2025-06-22 12:10:24 +0200

    Add missing prop

--- src/Modules/Hermes.Modules.Administration/Models/Users/UserDto.cs
+++ src/Modules/Hermes.Modules.Administration/Models/Users/UserDto.cs
@@ -8,6 +8,7 @@ namespace Hermes.Application.DTOs
     {
         public int Id { get; set; }
         public string Username { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
         public Role Role { get; set; }
         //public string Role { get; set; } = string.Empty; 
         public bool IsActive { get; set; }
