b52a97b7650c7b974da39a78f6abde9894bd5759
Karol Przybysz <przybyszkarol97@gmail.com>
2025-06-22 12:09:36 +0200

    modify update user command and handler

---
+++ src/Modules/Hermes.Modules.Administration/Commands/Users/UpdateUser/UpdateUserCommand.cs
@@ -0,0 +1,14 @@
+﻿using Hermes.Domain.Enums;
+using Hermes.Modules.Shared.Response;
+using MediatR;
+
+namespace Hermes.Modules.Administration.Commands.Users.UpdateUser
+{
+    public sealed class UpdateUserCommand : IRequest<Response>
+    {
+        public int Id { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public bool IsActive { get; set; }
+        public Role Role { get; set; }
+    }
+}
---
+++ src/Modules/Hermes.Modules.Administration/Commands/Users/UpdateUser/UpdateUserCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using Hermes.Domain.Entities;
+using Hermes.Modules.Shared.Handlers;
+using Hermes.Modules.Shared.Providers;
+using Hermes.Modules.Shared.Response;
+using Infrastructure.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hermes.Modules.Administration.Commands.Users.UpdateUser
+{
+    public sealed class UpdateUserCommandHandler : HandlerBase, IRequestHandler<UpdateUserCommand, Response>
+    {
+        public UpdateUserCommandHandler(IMapper mapper, IDateTimeProvider dateTimeProvider, AntiqueShopDbContext antiqueShopDbContext) : base(mapper, dateTimeProvider, antiqueShopDbContext)
+        {
+        }
+
+        public async Task<Response> Handle(UpdateUserCommand request, CancellationToken cancellationToken)
+        {
+            User user = await _context.Users.FirstOrDefaultAsync(u => u.Id == request.Id, cancellationToken);
+
+            if (user is null)
+            {
+                return Response.Failure($"User with a name: '{request.Username}' does not exist.");
+            }
+
+            user.Username = request.Username;
+            user.Role = request.Role;
+            user.IsActive = request.IsActive;
+
+            await _context.SaveChangesAsync();
+
+            return Response.Success();
+        }
+    }
+}
