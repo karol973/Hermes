715790e90113ed832421e5ce1fd33f4c7653dcea
Karol Przybysz <przybyszkarol97@gmail.com>
2025-06-24 01:00:49 +0200

    add routing

--- src/Hermes.Web/src/App.jsx
+++ src/Hermes.Web/src/App.jsx
@@ -5,6 +5,8 @@ import BookDetailsView from './views/BookDetailsView';
 import Login from './components/componentlogin.jsx';
 import Contact from './components/contactcomponent.jsx';
 import News from './components/newscomponent.jsx';
+import BooksCategory from './components/booksCategory.jsx';
+import Cart from './components/cart.jsx';
 
 const App = () => (
   <Routes>
@@ -13,6 +15,8 @@ const App = () => (
     <Route path="/login" element={<Login />} />
     <Route path="/contact" element={<Contact />} />
     <Route path="/news" element={<News />} />
+    <Route path="/cart" element={<Cart />} />
+    <Route path="/category/:id" element={<BooksCategory />} />
   </Routes>
 );
 
