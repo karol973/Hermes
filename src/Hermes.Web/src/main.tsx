8023c5eeaf649ead7d1c98a1d796ea9106245879
Karol Przybysz <przybyszkarol97@gmail.com>
2025-06-24 01:01:14 +0200

    Add cart provider

--- src/Hermes.Web/src/main.tsx
+++ src/Hermes.Web/src/main.tsx
@@ -5,13 +5,16 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 import { UserProvider } from './context/userContext';
+import { CartProvider } from './context/cartContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
+        <CartProvider>
         <App />
+        </CartProvider>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
